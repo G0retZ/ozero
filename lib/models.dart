4d6904dc275fda51609fbf9339b4437a8a23dc2d
Sergei Mitrofanov <goretz.m@gmail.com>
2020-06-07 07:20:46 +0300

    Make models composable & remove fields require

--- lib/models.dart
+++ lib/models.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/foundation.dart';
 
+/// Interface for merging fields of [D]
+abstract class Composable<D> {
+  /// returns new immutable instance that has non null fields of [other] instance
+  D mergeWith(D other);
+
+  D assertComposed();
+}
+
 class JsonEmptyChecker {
   JsonEmptyChecker.fromJson(Map<String, dynamic> json) {
     if (json.keys.length > 0) {
@@ -37,86 +45,124 @@ class Player extends JsonEmptyChecker {
       };
 }
 
-class Earning extends JsonEmptyChecker {
+class Earning extends JsonEmptyChecker implements Composable<Earning> {
   final int playerTurn;
   final int money;
   final int reputation;
 
-  Earning({
-    @required this.playerTurn,
-    @required this.money,
-    @required this.reputation,
-  }) : super.fromJson({});
+  Earning({this.playerTurn, this.money, this.reputation}) : super.fromJson({});
 
   Earning.fromJson(Map<String, dynamic> json)
       : playerTurn =
-  ArgumentError.checkNotNull(json.remove('playerTurn'), 'playerTurn'),
+            ArgumentError.checkNotNull(json.remove('playerTurn'), 'playerTurn'),
         money = ArgumentError.checkNotNull(json.remove('money'), 'money'),
         reputation =
-        ArgumentError.checkNotNull(json.remove('reputation'), 'reputation'),
+            ArgumentError.checkNotNull(json.remove('reputation'), 'reputation'),
         super.fromJson(json);
 
-  Map<String, dynamic> toJson() =>
-      {
+  Map<String, dynamic> toJson() => {
         'senderTurn': playerTurn,
         'money': money,
         'reputation': reputation,
       };
+
+  @override
+  Earning mergeWith(Earning other) {
+    return Earning(
+      playerTurn: other.playerTurn ?? playerTurn,
+      money: other.money ?? money,
+      reputation: other.reputation ?? reputation,
+    );
+  }
+
+  @override
+  Earning assertComposed() {
+    ArgumentError.checkNotNull(playerTurn, 'playerTurn');
+    ArgumentError.checkNotNull(money, 'money');
+    ArgumentError.checkNotNull(reputation, 'reputation');
+    return this;
+  }
 }
 
-class Remittance extends JsonEmptyChecker {
+class Remittance extends JsonEmptyChecker implements Composable<Remittance> {
   final int senderTurn;
   final int receiver;
   final int money;
 
-  Remittance({
-    @required this.senderTurn,
-    @required this.receiver,
-    @required this.money,
-  }) : super.fromJson({});
+  Remittance({this.senderTurn, this.receiver, this.money}) : super.fromJson({});
 
   Remittance.fromJson(Map<String, dynamic> json)
       : senderTurn =
-  ArgumentError.checkNotNull(json.remove('senderTurn'), 'senderTurn'),
+            ArgumentError.checkNotNull(json.remove('senderTurn'), 'senderTurn'),
         receiver =
-        ArgumentError.checkNotNull(json.remove('receiver'), 'senderTurn'),
+            ArgumentError.checkNotNull(json.remove('receiver'), 'senderTurn'),
         money = ArgumentError.checkNotNull(json.remove('money'), 'senderTurn'),
         super.fromJson(json);
 
-  Map<String, dynamic> toJson() =>
-      {
+  Map<String, dynamic> toJson() => {
         'senderTurn': senderTurn,
         'receiver': receiver,
         'money': money,
       };
+
+  @override
+  Remittance mergeWith(Remittance other) {
+    return Remittance(
+      senderTurn: other.senderTurn ?? senderTurn,
+      receiver: other.receiver ?? receiver,
+      money: other.money ?? money,
+    );
+  }
+
+  @override
+  Remittance assertComposed() {
+    ArgumentError.checkNotNull(senderTurn, 'senderTurn');
+    ArgumentError.checkNotNull(receiver, 'receiver');
+    ArgumentError.checkNotNull(money, 'money');
+    return this;
+  }
 }
 
-class DirtyTrick extends JsonEmptyChecker {
+class DirtyTrick extends JsonEmptyChecker implements Composable<DirtyTrick> {
   final int victimTurn;
   final int scum;
   final int fee;
   final int loss;
 
-  DirtyTrick({
-    @required this.victimTurn,
-    @required this.scum,
-    @required this.fee,
-    @required this.loss,
-  }) : super.fromJson({});
+  DirtyTrick({this.victimTurn, this.scum, this.fee, this.loss})
+      : super.fromJson({});
 
   DirtyTrick.fromJson(Map<String, dynamic> json)
       : victimTurn =
-  ArgumentError.checkNotNull(json.remove('victimTurn'), 'victimTurn'),
+            ArgumentError.checkNotNull(json.remove('victimTurn'), 'victimTurn'),
         scum = ArgumentError.checkNotNull(json.remove('scum'), 'scum'),
         fee = ArgumentError.checkNotNull(json.remove('fee'), 'fee'),
         loss = ArgumentError.checkNotNull(json.remove('loss'), 'loss'),
         super.fromJson(json);
 
-  Map<String, dynamic> toJson() =>
-      {
+  Map<String, dynamic> toJson() => {
         'victimTurn': victimTurn,
         'scum': scum,
         'fee': fee,
         'loss': loss,
       };
+
+  @override
+  DirtyTrick mergeWith(DirtyTrick other) {
+    return DirtyTrick(
+      victimTurn: other.victimTurn ?? victimTurn,
+      scum: other.scum ?? scum,
+      fee: other.fee ?? fee,
+      loss: other.loss ?? loss,
+    );
+  }
+
+  @override
+  DirtyTrick assertComposed() {
+    ArgumentError.checkNotNull(victimTurn, 'victimTurn');
+    ArgumentError.checkNotNull(scum, 'scum');
+    ArgumentError.checkNotNull(fee, 'fee');
+    ArgumentError.checkNotNull(loss, 'loss');
+    return this;
+  }
 }
--- test/models.dart
+++ test/models.dart
@@ -106,6 +106,156 @@ void main() {
               e is UnsupportedError &&
               e.message == 'unknonw fields (a, c, e)'));
     });
+
+    test('merge empty with other empty', () {
+      // Given:
+      final original = Earning(
+        playerTurn: null,
+        money: null,
+        reputation: null,
+      );
+      final other = Earning(
+        playerTurn: null,
+        money: null,
+        reputation: null,
+      );
+
+      // When:
+      final result = original.mergeWith(other);
+
+      // Then:
+      expect(result.playerTurn, null);
+      expect(result.money, null);
+      expect(result.reputation, null);
+    });
+
+    test('merge empty with other filled', () {
+      // Given:
+      final original = Earning(
+        playerTurn: null,
+        money: null,
+        reputation: null,
+      );
+      final other = Earning(
+        playerTurn: 3,
+        money: 4,
+        reputation: 5,
+      );
+
+      // When:
+      final result = original.mergeWith(other);
+
+      // Then:
+      expect(result.playerTurn, 3);
+      expect(result.money, 4);
+      expect(result.reputation, 5);
+    });
+
+    test('merge filled with other empty', () {
+      // Given:
+      final original = Earning(
+        playerTurn: 0,
+        money: 1,
+        reputation: 2,
+      );
+      final other = Earning(
+        playerTurn: null,
+        money: null,
+        reputation: null,
+      );
+
+      // When:
+      final result = original.mergeWith(other);
+
+      // Then:
+      expect(result.playerTurn, 0);
+      expect(result.money, 1);
+      expect(result.reputation, 2);
+    });
+
+    test('merge filled with other filled', () {
+      // Given:
+      final original = Earning(
+        playerTurn: 0,
+        money: 1,
+        reputation: 2,
+      );
+      final other = Earning(
+        playerTurn: 3,
+        money: 4,
+        reputation: 5,
+      );
+
+      // When:
+      final result = original.mergeWith(other);
+
+      // Then:
+      expect(result.playerTurn, 3);
+      expect(result.money, 4);
+      expect(result.reputation, 5);
+    });
+
+    test('assert composed failed without playerTurn', () {
+      // Given:
+      final earning = Earning(
+        playerTurn: null,
+        money: 1,
+        reputation: 2,
+      );
+
+      // When and Then:
+      expect(
+          earning.assertComposed,
+          throwsA(
+              (e) => e is ArgumentError && e.message == 'Must not be null'));
+    });
+
+    test('assert composed failed without money', () {
+      // Given:
+      final earning = Earning(
+        playerTurn: 0,
+        money: null,
+        reputation: 2,
+      );
+
+      // When and Then:
+      expect(
+          earning.assertComposed,
+          throwsA(
+              (e) => e is ArgumentError && e.message == 'Must not be null'));
+    });
+
+    test('assert composed failed without reputation', () {
+      // Given:
+      final earning = Earning(
+        playerTurn: 0,
+        money: 1,
+        reputation: null,
+      );
+
+      // When and Then:
+      expect(
+          earning.assertComposed,
+          throwsA(
+              (e) => e is ArgumentError && e.message == 'Must not be null'));
+    });
+
+    test('assert composed success', () {
+      // Given:
+      final earning = Earning(
+        playerTurn: 0,
+        money: 1,
+        reputation: 2,
+      );
+
+      // When:
+      final result = earning.assertComposed();
+
+      // Then:
+      expect(result.playerTurn, 0);
+      expect(result.money, 1);
+      expect(result.reputation, 2);
+    });
   });
 
   group('test Remittance model', () {
@@ -153,6 +303,156 @@ void main() {
           throwsA((e) =>
               e is UnsupportedError &&
               e.message == 'unknonw fields (a, c, e)'));
+    });
+
+    test('merge empty with other empty', () {
+      // Given:
+      final original = Remittance(
+        senderTurn: null,
+        receiver: null,
+        money: null,
+      );
+      final other = Remittance(
+        senderTurn: null,
+        receiver: null,
+        money: null,
+      );
+
+      // When:
+      final result = original.mergeWith(other);
+
+      // Then:
+      expect(result.senderTurn, null);
+      expect(result.receiver, null);
+      expect(result.money, null);
+    });
+
+    test('merge empty with other filled', () {
+      // Given:
+      final original = Remittance(
+        senderTurn: null,
+        receiver: null,
+        money: null,
+      );
+      final other = Remittance(
+        senderTurn: 3,
+        receiver: 4,
+        money: 5,
+      );
+
+      // When:
+      final result = original.mergeWith(other);
+
+      // Then:
+      expect(result.senderTurn, 3);
+      expect(result.receiver, 4);
+      expect(result.money, 5);
+    });
+
+    test('merge filled with other empty', () {
+      // Given:
+      final original = Remittance(
+        senderTurn: 0,
+        receiver: 1,
+        money: 2,
+      );
+      final other = Remittance(
+        senderTurn: null,
+        receiver: null,
+        money: null,
+      );
+
+      // When:
+      final result = original.mergeWith(other);
+
+      // Then:
+      expect(result.senderTurn, 0);
+      expect(result.receiver, 1);
+      expect(result.money, 2);
+    });
+
+    test('merge filled with other filled', () {
+      // Given:
+      final original = Remittance(
+        senderTurn: 0,
+        receiver: 1,
+        money: 2,
+      );
+      final other = Remittance(
+        senderTurn: 3,
+        receiver: 4,
+        money: 5,
+      );
+
+      // When:
+      final result = original.mergeWith(other);
+
+      // Then:
+      expect(result.senderTurn, 3);
+      expect(result.receiver, 4);
+      expect(result.money, 5);
+    });
+
+    test('assert composed failed without senderTurn', () {
+      // Given:
+      final remittance = Remittance(
+        senderTurn: null,
+        receiver: 1,
+        money: 2,
+      );
+
+      // When and Then:
+      expect(
+          remittance.assertComposed,
+          throwsA(
+              (e) => e is ArgumentError && e.message == 'Must not be null'));
+    });
+
+    test('assert composed failed without receiver', () {
+      // Given:
+      final remittance = Remittance(
+        senderTurn: 0,
+        receiver: null,
+        money: 2,
+      );
+
+      // When and Then:
+      expect(
+          remittance.assertComposed,
+          throwsA(
+              (e) => e is ArgumentError && e.message == 'Must not be null'));
+    });
+
+    test('assert composed failed without money', () {
+      // Given:
+      final remittance = Remittance(
+        senderTurn: 0,
+        receiver: 1,
+        money: null,
+      );
+
+      // When and Then:
+      expect(
+          remittance.assertComposed,
+          throwsA(
+              (e) => e is ArgumentError && e.message == 'Must not be null'));
+    });
+
+    test('assert composed success', () {
+      // Given:
+      final remittance = Remittance(
+        senderTurn: 0,
+        receiver: 1,
+        money: 2,
+      );
+
+      // When:
+      final result = remittance.assertComposed();
+
+      // Then:
+      expect(result.senderTurn, 0);
+      expect(result.receiver, 1);
+      expect(result.money, 2);
     });
   });
 
@@ -202,6 +502,189 @@ void main() {
           throwsA((e) =>
               e is UnsupportedError &&
               e.message == 'unknonw fields (a, c, e)'));
+    });
+
+    test('merge empty with other empty', () {
+      // Given:
+      final original = DirtyTrick(
+        victimTurn: null,
+        scum: null,
+        fee: null,
+        loss: null,
+      );
+      final other = DirtyTrick(
+        victimTurn: null,
+        scum: null,
+        fee: null,
+        loss: null,
+      );
+
+      // When:
+      final result = original.mergeWith(other);
+
+      // Then:
+      expect(result.victimTurn, null);
+      expect(result.scum, null);
+      expect(result.fee, null);
+      expect(result.loss, null);
+    });
+
+    test('merge empty with other filled', () {
+      // Given:
+      final original = DirtyTrick(
+        victimTurn: null,
+        scum: null,
+        fee: null,
+        loss: null,
+      );
+      final other = DirtyTrick(
+        victimTurn: 3,
+        scum: 4,
+        fee: 5,
+        loss: 6,
+      );
+
+      // When:
+      final result = original.mergeWith(other);
+
+      // Then:
+      expect(result.victimTurn, 3);
+      expect(result.scum, 4);
+      expect(result.fee, 5);
+      expect(result.loss, 6);
+    });
+
+    test('merge filled with other empty', () {
+      // Given:
+      final original = DirtyTrick(
+        victimTurn: 0,
+        scum: 1,
+        fee: 2,
+        loss: 3,
+      );
+      final other = DirtyTrick(
+        victimTurn: null,
+        scum: null,
+        fee: null,
+        loss: null,
+      );
+
+      // When:
+      final result = original.mergeWith(other);
+
+      // Then:
+      expect(result.victimTurn, 0);
+      expect(result.scum, 1);
+      expect(result.fee, 2);
+      expect(result.loss, 3);
+    });
+
+    test('merge filled with other filled', () {
+      // Given:
+      final original = DirtyTrick(
+        victimTurn: 0,
+        scum: 1,
+        fee: 2,
+        loss: 3,
+      );
+      final other = DirtyTrick(
+        victimTurn: 3,
+        scum: 4,
+        fee: 5,
+        loss: 6,
+      );
+
+      // When:
+      final result = original.mergeWith(other);
+
+      // Then:
+      expect(result.victimTurn, 3);
+      expect(result.scum, 4);
+      expect(result.fee, 5);
+      expect(result.loss, 6);
+    });
+
+    test('assert composed failed without victimTurn', () {
+      // Given:
+      final dirtyTrick = DirtyTrick(
+        victimTurn: null,
+        scum: 1,
+        fee: 2,
+        loss: 3,
+      );
+
+      // When and Then:
+      expect(
+          dirtyTrick.assertComposed,
+          throwsA(
+              (e) => e is ArgumentError && e.message == 'Must not be null'));
+    });
+
+    test('assert composed failed without scum', () {
+      // Given:
+      final dirtyTrick = DirtyTrick(
+        victimTurn: 0,
+        scum: null,
+        fee: 2,
+        loss: 3,
+      );
+
+      // When and Then:
+      expect(
+          dirtyTrick.assertComposed,
+          throwsA(
+              (e) => e is ArgumentError && e.message == 'Must not be null'));
+    });
+
+    test('assert composed failed without fee', () {
+      // Given:
+      final dirtyTrick = DirtyTrick(
+        victimTurn: 0,
+        scum: 1,
+        fee: null,
+        loss: 3,
+      );
+
+      // When and Then:
+      expect(
+          dirtyTrick.assertComposed,
+          throwsA(
+              (e) => e is ArgumentError && e.message == 'Must not be null'));
+    });
+
+    test('assert composed failed without loss', () {
+      // Given:
+      final dirtyTrick = DirtyTrick(
+        victimTurn: 0,
+        scum: 1,
+        fee: 2,
+        loss: null,
+      );
+
+      // When and Then:
+      expect(
+          dirtyTrick.assertComposed,
+          throwsA(
+              (e) => e is ArgumentError && e.message == 'Must not be null'));
+    });
+
+    test('assert composed success', () {
+      // Given:
+      final dirtyTrick = DirtyTrick(
+        victimTurn: 0,
+        scum: 1,
+        fee: 2,
+        loss: 3,
+      );
+
+      // When:
+      final result = dirtyTrick.assertComposed();
+
+      // Then:
+      expect(result.victimTurn, 0);
+      expect(result.scum, 1);
+      expect(result.fee, 2);
+      expect(result.loss, 3);
     });
   });
 }
